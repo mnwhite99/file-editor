deb36d89b3a2c096f8bd52dd043819c41a30fbbc
mnwhite99 <mnwhite@uchicago.edu>
2021-03-06 05:25:39 -0600

    code

--- asm/source/main.js
+++ asm/source/main.js
@@ -1,7 +1,10 @@
+let floatBuffer
+let IEEE7542019
+let opcodes
 let code
-let symbols
-let symbolPosition
-let symbol
+let letters
+let letterPosition
+let letter
 let newLinePosition
 let inlinePosition
 let destination
@@ -12,31 +15,41 @@ let immediate
 let operand_1
 let operator
 let operand_2
-let opcodes      = {             }
- opcodes [ '+' ] = [ 0xA0        ]
- opcodes [ '-' ] = [ 0xA1        ]
- opcodes [ '*' ] = [ 0xA2        ]
- opcodes [ '/' ] = [ 0xA3        ]
- opcodes [ '&' ] = [ 0xA4        ]
- opcodes [ '|' ] = [ 0xA5        ]
- opcodes [ '"' ] = [ 0xA6        ]
- opcodes [ '_' ] = [ 0xA0 , 0x9C ]
- opcodes [ '~' ] = [ 0xA0 , 0x9E ]
- opcodes [ '?' ] = [ 0xA0 , 0x9F ]
-let floatBuffer  = new DataView ( new Float64Array ( 1 ) [ 'buffer' ] )
-document [ 'getElementById' ] ( 'symbols' ) [ 'oninput' ] = function ( ) {
- code           = [ ]
- symbols        = document [ 'getElementById' ] ( 'symbols' )
- symbolPosition = 1
- localStorage [ 'setItem' ] ( 'symbols' , symbols [ 'value' ] )
- while ( symbolPosition <= symbols [ 'value' ] [ 'length' ] ) {
-  symbol = symbols [ 'value' ] [ symbolPosition - 1 ]
-  if ( symbol === '\n' ) {
-   newLinePosition = symbolPosition
+floatBuffer = new DataView(new Float64Array(1)['buffer'])
+IEEE7542019 = function(value) {
+ return floatBuffer['setFloat64'](parseFloat(value), 1)['getFloat64'](0, 1)['buffer']
+}
+opcodes      = {          }
+opcodes['='] = [0x61, 0xb7]
+opcodes['!'] = [0x62, 0xb7]
+opcodes['<'] = [0x63, 0xb7]
+opcodes['>'] = [0x64, 0xb7]
+opcodes['['] = [0x65, 0xb7]
+opcodes[']'] = [0x66, 0xb7]
+opcodes['+'] = [0xA0      ]
+opcodes['-'] = [0xA1      ]
+opcodes['*'] = [0xA2      ]
+opcodes['/'] = [0xA3      ]
+opcodes['&'] = [0xA4      ]
+opcodes['|'] = [0xA5      ]
+opcodes['"'] = [0xA6      ]
+opcodes['_'] = [0xA0, 0x9C]
+opcodes['~'] = [0xA0, 0x9E]
+opcodes['?'] = [0xA0, 0x9F]
+document['getElementById']('letters')['oninput'] = function() {
+ code            = []
+ letters         = document['getElementById']('letters')
+ letterPosition  = 1
+ newLinePosition = 1
+ localStorage['setItem']('letters', letters['value'])
+ while (letterPosition <= letters['value']['length']) {
+  letter = letters['value'][letterPosition - 1]
+  if (letter === '\n') {
+   newLinePosition = letterPosition
   }
-  inlinePosition = symbolPosition - newLinePosition
-  if ( inlinePosition === 0 ) {
-   destination = ' '
+  inlinePosition = letterPosition - newLinePosition
+  if (inlinePosition === 0) {
+   destination =  undefined
    error       =  0
    read        =  0
    write       =  0
@@ -48,52 +61,53 @@ document [ 'getElementById' ] ( 'symbols' ) [ 'oninput' ] = function ( ) {
    operator    = '+'
    operand_2   = '0'
   }
-  else if ( inlinePosition === 1                   ) destination = symbol
-  else if ( inlinePosition === 2 && symbol !== ' ' ) error       = 1
-  else if ( inlinePosition === 3 && symbol === '(' ) read        = 1
-  else if ( inlinePosition === 3 && symbol === ')' ) write       = 1
-  else if ( inlinePosition === 3 && symbol !== '=' ) error       = 1
-  else if ( inlinePosition === 4 && symbol !== ' ' ) error       = 1
-  else if ( inlinePosition === 5 && symbol === '$' ) immediate   = 1
-  else if ( inlinePosition === 5                   ) operand_1   = symbol
-  else if ( inlinePosition === 6 && symbol !== ' ' ) error       = 1
-  else if ( inlinePosition === 7                   ) operator    = symbol
-  else if ( inlinePosition === 8 && symbol !== ' ' ) error       = 1
-  else if ( inlinePosition === 9                   ) operand_2   = symbol
-  if      ( opcodes [ operator ] === undefined ) error = 1
-  if      ( error === 1 ) return
-  else if ( immediate === 0 && symbol === '\n' ) {
-   code [ 'concat' ] (
-    [ 0x23 ] ,
-    [ operand_1 [ 'charCodeAt' ] ( 0 ) ] ,
-    [ 0x23 ] ,
-    [ operand_2 [ 'charCodeAt' ] ( 0 ) ] ,
-    opcodes [ operator ]
-   )
+  else if (inlinePosition === 1                  ) destination = letter
+  else if (inlinePosition === 2 && letter !== ' ') error       = 1
+  else if (inlinePosition === 3 && letter === '{') read        = 1
+  else if (inlinePosition === 3 && letter === '}') write       = 1
+  else if (inlinePosition === 3 && letter !== '=') error       = 1
+  else if (inlinePosition === 4 && letter !== ' ') error       = 1
+  else if (inlinePosition === 5 && letter === '$') immediate   = 1
+  else if (inlinePosition === 5                  ) operand_1   = letter
+  else if (inlinePosition === 6 && letter !== ' ') error       = 1
+  else if (inlinePosition === 7                  ) operator    = letter
+  else if (inlinePosition === 8 && letter !== ' ') error       = 1
+  else if (inlinePosition === 9                  ) operand_2   = letter
+  if (opcodes[operator] === undefined) {
+   error = 1
   }
-  else if ( immediate === 1 && symbol !== ' '  ) {
-   operand_1 = operand_1 + symbol
+  if (destination !== undefined && error === 0 && (letter === '\n' || letterPosition === letters['length'])) {
+   if (immediate === 1) {
+    code['concat'](IEEE7542019(operand_1))
+   }
+   else {
+    code['concat']([operand_1['charCodeAt'](0), 0x23, operand_2['charCodeAt'](0), 0x23, opcodes[operator]])
+   }
+   if      (read  === 1) {
+    code['concat']([0xb4, 0x40, 0x2b, destination['charCodeAt'](0), 0x24])
+   }
+   else if (write === 1) {
+    code['concat']([0xb4, 0x40, destination['charCodeAt'](0), 0x23, 0xb7])
+   }
+   else {
+    code['concat']([destination['charCodeAt'](0), 0x24])
+   }
   }
-  else if ( immediate === 1 && symbol === '\n' ) {
-   operand_1 = parseFloat ( operand_1 )
-   floatBuffer [ 'setFloat64' ] ( operand_1 , 1 )
-   code [ 'concat' ] (
-    [ 0x44 ] ,
-    floatBuffer [ 'getFloat64' ] ( 0 , 1 ) [ 'buffer' ] ,
-    [ 0x24 ] ,
-    [ destination [ 'charCodeAt' ] ( 0 ) ]
-   )
+  else if (immediate === 1 && letter !== ' ' ) {
+   operand_1 = operand_1 + letter
   }
+  letterPosition = letterPosition + 1
  }
 }
-window [ 'onload' ] = function ( ) {
- symbols                     = document [ 'getElementById' ] ( 'symbols' )
- symbols [ 'innerHTML'     ] = localStorage [ 'getItem' ] ( 'symbols' )
- symbols [ 'oncut'         ] =
- symbols [ 'oncopy'        ] =
- symbols [ 'onpaste'       ] =
- symbols [ 'ondrag'        ] =
- symbols [ 'ondrop'        ] =
- symbols [ 'onpointermove' ] =
- symbols [ 'oncontextmenu' ] = function ( event ) { event [ 'preventDefault' ] ( ) }
+window['onload'] = function() {
+ letters                  = document['getElementById']('letters')
+ letters['innerHTML'    ] = localStorage['getItem']('letters')
+ letters['oncut'        ] =
+ letters['oncopy'       ] =
+ letters['onpaste'      ] =
+ letters['ondrag'       ] =
+ letters['ondrop'       ] =
+ letters['onpointermove'] =
+ letters['oncontextmenu'] = function(event) { event['preventDefault']() }
+ letters['onselect'     ] = function() { window['getSelection']()['collapseToStart']() }
 }
